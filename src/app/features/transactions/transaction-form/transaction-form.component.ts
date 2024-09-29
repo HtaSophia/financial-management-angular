aba0915b115c848a9313dec416b90ba16d672401
Aghata Sophia <aghatasophia@gmail.com>
2024-09-29 16:59:33 -0400

    feat(transaction): Init - Create Transaction Form component

---
+++ src/app/features/transactions/transaction-form/transaction-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'fm-transaction-form',
+    standalone: true,
+    imports: [],
+    templateUrl: './transaction-form.component.html',
+    styleUrl: './transaction-form.component.scss',
+})
+export class TransactionFormComponent {}
