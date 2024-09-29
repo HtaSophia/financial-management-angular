b841bcc09cd9d76ef4ef280bd637fcc5b7e25bf8
Aghata Sophia <aghatasophia@gmail.com>
2024-09-29 10:53:21 -0400

    feat(app): Update and clean the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-root',
+    selector: 'fm-root',
     standalone: true,
     imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
-export class AppComponent {
-    title = 'financial-management-angular';
-}
+export class AppComponent {}
