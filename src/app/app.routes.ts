7c47c5570e7a093a6a2d7c737083b5702c2c80b7
Aghata Sophia <aghatasophia@gmail.com>
2024-09-29 17:00:16 -0400

    feat(app): Add routes to the new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/auth/pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        component: LoginComponent,
+    },
+    {
+        path: '',
+        loadComponent: () => import('./core/layout/nav-layout.component').then((mod) => mod.NavLayoutComponent),
+        children: [
+            {
+                path: 'dashboard',
+                title: 'Dashboard',
+                loadComponent: () =>
+                    import('./features/dashboard/dashboard.component').then((mod) => mod.DashboardComponent),
+            },
+            {
+                path: 'transactions',
+                loadChildren: () => import('./features/transactions/transactions.routes'),
+            },
+        ],
+    },
+];
