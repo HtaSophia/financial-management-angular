877b80f28f722578756526944a2d768e19c1db6c
Aghata Sophia <aghatasophia@gmail.com>
2024-10-01 16:01:38 -0400

    feat(transaction): Add create transaction method in the transaction service

--- src/app/features/transactions/shared/transaction.service.ts
+++ src/app/features/transactions/shared/transaction.service.ts
@@ -5,7 +5,16 @@ import { TRANSACTIONS_DATA } from '../../../shared/mocks/transactions.mock';
 
 @Injectable()
 export class TransactionService {
+    private transactions = [...TRANSACTIONS_DATA];
+
+    public create(transaction: Transaction): Observable<Transaction> {
+        const lastId = this.transactions.at(-1)?.id ?? '0';
+        const id = `${parseInt(lastId) + 1}`;
+        this.transactions.push({ ...transaction, id });
+        return of(transaction).pipe(delay(1000));
+    }
+
     public getAll(): Observable<Transaction[]> {
-        return of(TRANSACTIONS_DATA).pipe(delay(1000));
+        return of(this.transactions).pipe(delay(1000));
     }
 }
