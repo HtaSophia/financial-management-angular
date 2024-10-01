8fa4469661a2e3495d2498b99c3b0157d2cd83a9
Aghata Sophia <aghatasophia@gmail.com>
2024-10-01 18:23:49 -0400

    feat(auth): Add user model

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { User } from './user.model';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    public get user() {
-        return localStorage.getItem('user');
+    public get user(): User | null {
+        const currentUser = localStorage.getItem('user');
+        if (currentUser) return JSON.parse(currentUser);
+
+        return null;
     }
 
     public login(email: string): void {
