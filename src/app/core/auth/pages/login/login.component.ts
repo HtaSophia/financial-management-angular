b2cabead2c44eda1435e49052b6a41df41846ce0
Aghata Sophia <aghatasophia@gmail.com>
2024-10-01 11:57:10 -0400

    feat(login): Create the login form and add the auth service

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,10 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import { AuthService } from '../../auth.service';
 
 @Component({
     selector: 'fm-login',
     standalone: true,
+    imports: [FormsModule, ReactiveFormsModule, RouterModule, MatFormFieldModule, MatInputModule, MatButtonModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+    public emailControl = new FormControl('', [Validators.required, Validators.email]);
+    public passwordControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+
+    private readonly authService = inject(AuthService);
+    private readonly router = inject(Router);
+
+    public onSubmit(): void {
+        this.authService.login(this.emailControl.value!);
+        this.router.navigate(['dashboard']);
+    }
+}
