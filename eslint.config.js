a206ac7ca894d5b45e075586b2d2515d8cc081a0
Aghata Sophia <aghatasophia@gmail.com>
2024-10-02 10:02:31 -0400

    lint: Update no-unused-vars rule

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,18 @@ module.exports = tseslint.config(
         ],
         processor: angular.processInlineTemplates,
         rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    args: 'all',
+                    argsIgnorePattern: '^_',
+                    caughtErrors: 'all',
+                    caughtErrorsIgnorePattern: '^_',
+                    destructuredArrayIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    ignoreRestSiblings: true,
+                },
+            ],
             '@angular-eslint/directive-selector': [
                 'error',
                 {
